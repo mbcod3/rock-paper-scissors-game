afc93881358be4f88910eec842ee059791f8dd6e
mbcod3 <58457269+mbcod3@users.noreply.github.com>
2020-01-20 21:42:54 +0500

    Updates

---
+++ precache-manifest.bb1e31f639da0298d976c988156e00c3.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9af8078d32abd25b775da5f7f15ce36",
+    "url": "/rock-paper-scissors-game/index.html"
+  },
+  {
+    "revision": "153f9e813ffd002678c2",
+    "url": "/rock-paper-scissors-game/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "2ed41ac8dbbb54fb15d0",
+    "url": "/rock-paper-scissors-game/static/js/2.be365a10.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/rock-paper-scissors-game/static/js/2.be365a10.chunk.js.LICENSE"
+  },
+  {
+    "revision": "153f9e813ffd002678c2",
+    "url": "/rock-paper-scissors-game/static/js/main.c0d77506.chunk.js"
+  },
+  {
+    "revision": "4dcaa7c45dc3ae5cd1b9",
+    "url": "/rock-paper-scissors-game/static/js/runtime-main.a683337a.js"
+  },
+  {
+    "revision": "aced70522265bc609155bea44802776b",
+    "url": "/rock-paper-scissors-game/static/media/bg-pentagon.aced7052.svg"
+  },
+  {
+    "revision": "1fd44bdf9274b331fe659004ccc71e63",
+    "url": "/rock-paper-scissors-game/static/media/bg-triangle.1fd44bdf.svg"
+  },
+  {
+    "revision": "7d2e0b18d2860b90407f41297dce461f",
+    "url": "/rock-paper-scissors-game/static/media/icon-lizard.7d2e0b18.svg"
+  },
+  {
+    "revision": "69a8c2e96b659b35a3760c79123be610",
+    "url": "/rock-paper-scissors-game/static/media/icon-paper.69a8c2e9.svg"
+  },
+  {
+    "revision": "8ce1e109bb43912192f5ba8dfcca005e",
+    "url": "/rock-paper-scissors-game/static/media/icon-rock.8ce1e109.svg"
+  },
+  {
+    "revision": "bec2f9b3d60cb80987106bb49205c53f",
+    "url": "/rock-paper-scissors-game/static/media/icon-scissors.bec2f9b3.svg"
+  },
+  {
+    "revision": "b6446e8e1e5d5609deb209d401e779a3",
+    "url": "/rock-paper-scissors-game/static/media/icon-spock.b6446e8e.svg"
+  },
+  {
+    "revision": "f19a40fbc5296effc230454fa1295890",
+    "url": "/rock-paper-scissors-game/static/media/image-rules-bonus.f19a40fb.svg"
+  },
+  {
+    "revision": "5d74d25d9c64bde346585ec7e7587a69",
+    "url": "/rock-paper-scissors-game/static/media/image-rules.5d74d25d.svg"
+  },
+  {
+    "revision": "f4c973f743c8b75de9da23335e126dcd",
+    "url": "/rock-paper-scissors-game/static/media/logo-bonus.f4c973f7.svg"
+  },
+  {
+    "revision": "759a207509c8dad61b7e365b27e0d0bd",
+    "url": "/rock-paper-scissors-game/static/media/logo.759a2075.svg"
+  }
+]);
